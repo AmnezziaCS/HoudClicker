68096589e07ec93ad368e4f3411c505518798caa
Seelegen <martin@tdrapied>
2024-09-05 14:01:16 +0200

    feat(app): Add Music on Lauch

--- HoudClicker/BackgroundMusicPlayer.swift
+++ HoudClicker/BackgroundMusicPlayer.swift
@@ -9,13 +9,15 @@ class BackgroundMusicPlayer {
         if let url = Bundle.main.url(forResource: filename, withExtension: fileType) {
             do {
                 audioPlayer = try AVAudioPlayer(contentsOf: url)
-                audioPlayer?.numberOfLoops = -1
+                audioPlayer?.numberOfLoops = 1
                 audioPlayer?.prepareToPlay()
                 audioPlayer?.play()
+                return
             } catch {
                 print("Erreur lors de la lecture de la musique : \(error.localizedDescription)")
             }
         }
+        print("couldnt load file")
     }
     
     func stopMusic() {
