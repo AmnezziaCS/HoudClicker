7587d198159dcdb38426778d7befd09786278b25
Mathys Visticot <mathys.vst@gmail.com>
2024-09-05 16:11:34 +0200

    feat(app): add popup

--- HoudClicker/ViewController.swift
+++ HoudClicker/ViewController.swift
@@ -9,19 +9,22 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    
-    @IBAction func onClickButton(_ sender: UIButton) {
-        print("C'est bien cliqué")
-        BackgroundMusicPlayer.shared.playMusic(filename: "BackgroundMusic")
-    }
-    
+
     @IBOutlet weak var conteur: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+
+    @IBAction func onClickButton(_ sender: UIButton) {
+        BackgroundMusicPlayer.shared.playMusic(filename: "BackgroundMusic")
+        let alert = UIAlertController(title: "Attention", message: "Frappe plus!", preferredStyle: .alert)
         
-        
+        self.present(alert, animated: true) {
+            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) { _ in
+                alert.dismiss(animated: true, completion: nil)
+            }
+        }
     }
 }
-
-
